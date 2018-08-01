6f2bd04931123b1171fcebcb25737e2762bea3f1
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-01 20:51:29 +0600

    Create assignmentOperators.js

---
+++ assignmentOperators.js
@@ -0,0 +1,13 @@
+/************************
+  Assignment Operators
+  =  +=  -=  *=  /=  %=
+ ************************/
+
+var x = 4;
+// x += 2;//x = x+2//Output 6
+// x-=2;//x = x-2//Output 2
+// x*=2;//x = x*2//Output 8
+// x/=2;//x = x/2 //Output 2
+// x%=2;//x = x%2 //Output 0
+
+console.log(x);//Output 0

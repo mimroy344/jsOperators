d6950c83a32e115c6f7e5392908d506fdbebe87c
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-17 20:46:18 +0600

    Create @typeOfJavascript.js

---
+++ @typeOfJavascript.js
@@ -0,0 +1,23 @@
+/***************************************
+         Type Of Javascript 
+**************************************/
+1.Compound assignment Operator(13)
+  // =,+=,-=,*=,/=,%=,**,&=,|=,^=,<<=,>>=,>>>=
+2.Comparison Operator(8)
+  // ==,!=,===,!==,<,>,<=,>=
+3.Aritmetic Operator(8)
+  // +,-,*,/,%,++,--,**
+4.Bitwise Operator(7)
+  // &,|,^,~,<<,>>,>>>
+5.Logical Operator(3)
+  // &&,||,!
+6.String Operator(1)
+  // +
+7.Condition(ternary) Operator(1)
+  // ?:
+8.Comma Operator(1)
+  // ,
+9.Unary Operator(3)
+  // void,delete,typeof
+10.Relational Operator(2)
+  // in,instanceof
